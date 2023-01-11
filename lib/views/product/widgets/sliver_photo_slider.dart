90cdeab397c832cdc9e4a5bf26868b34c78516d4
Mohamed Mamdoh <mamdoh@3x1.io>
2023-01-11 11:18:02 +0200

    .

--- lib/views/product/widgets/sliver_photo_slider.dart
+++ lib/views/product/widgets/sliver_photo_slider.dart
@@ -51,7 +51,7 @@ class SliverPhotoSlider extends HookConsumerWidget {
             ),
           ),
           Padding(
-            padding: const EdgeInsets.only(right: 20),
+            padding: const EdgeInsets.symmetric(horizontal: 20),
             child: Row(
               children: [
                 CircleAvatar(
